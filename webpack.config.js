6adc929d7c48a9df894ecb1279f776b3e0f282b6
fullstop125 <hassanmoses125@gmail.com>
2022-08-31 13:21:08 +0300

    add header

--- webpack.config.js
+++ webpack.config.js
@@ -12,6 +12,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      favicon: './src/icons&imgs/dynasty-logo-plain.png',
     }),
   ],
   output: {
@@ -28,6 +29,10 @@ module.exports = {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+       }
     ],
   },
 };
