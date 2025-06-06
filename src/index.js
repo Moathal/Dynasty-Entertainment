4911cbdd48ca6b884b48cca1bea96c6f23272b56
Moathal <moathal.s.kachi@gmail.com>
2025-06-06 16:26:28 +0300

    Fix initial loading to show the modal

--- src/index.js
+++ src/index.js
@@ -17,10 +17,8 @@ const comment = document.getElementById("commentToPost");
 const small = document.getElementById("small");
 
 function render() {
-	container.innerHTML = `<div class="loadingDiv" style="height: 100vh; width: 100%; display: flex; justify-content: center; align-items: center">
-    </div>`;
 	const loadingDiv = document.querySelector(".loadingDiv");
-	loadingDiv.innerHTML = `<div style="min-height: 100%; width: 30%; display: flex; justify-content: center; align-items: center"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 150"><path fill="none" stroke="#F12226" stroke-width="30" stroke-linecap="round" stroke-dasharray="300 385" stroke-dashoffset="0" d="M275 75c0 31-27 50-50 50-58 0-92-100-150-100-28 0-50 22-50 50s23 50 50 50c58 0 92-100 150-100 24 0 50 19 50 50Z"><animate attributeName="stroke-dashoffset" calcMode="spline" dur="2" values="685;-685" keySplines="0 0 1 1" repeatCount="indefinite"></animate></path></svg></div>`;
+	loadingDiv.setAttribute("style", "display: flex");
 	methods.loadData(container, animesAPI, likesAPI);
 }
 
--- src/modules/methods.js
+++ src/modules/methods.js
@@ -1,23 +1,29 @@
 import {
-  getAnime, getComments, postComments, getLikes,
-} from './APIsGET&POST.js';
+	getAnime,
+	getComments,
+	postComments,
+	getLikes,
+} from "./APIsGET&POST.js";
 
 export default class Methods {
-  constructor() {
-    this.animes = [];
-  }
+	constructor() {
+		this.animes = [];
+	}
 
-  async loadData(movieList, animeAPI, likesAPI) {
-    const list = await getAnime(animeAPI);
-    const likes = await getLikes(likesAPI);
-    if (Array.isArray(list)) movieList.innerHTML = '';
-    list.forEach((anime, i) => {
-      const li = document.createElement('li');
-      let like = [];
-      like = likes.filter((item) => item.item_id === `${i}`);
-      let liked = 0;
-      if (like.length > 0) liked = like[0].likes;
-      li.innerHTML = `<img src="${anime.image}"><h2 id="title-${i}">${anime.title}</h2>
+	async loadData(movieList, animeAPI, likesAPI) {
+		const list = await getAnime(animeAPI);
+		const likes = await getLikes(likesAPI);
+		if (Array.isArray(list));
+		document
+			.querySelector(".loadingDiv")
+			.setAttribute("style", "display: none");
+		list.forEach((anime, i) => {
+			const li = document.createElement("li");
+			let like = [];
+			like = likes.filter((item) => item.item_id === `${i}`);
+			let liked = 0;
+			if (like.length > 0) liked = like[0].likes;
+			li.innerHTML = `<img src="${anime.image}"><h2 id="title-${i}">${anime.title}</h2>
                   <div class="container">
                   <button id="cmntb-${i}"class="learn-more" data-bs-toggle="modal" data-bs-target="#exampleModal">
                     <span class="circle" aria-hidden="true">
@@ -37,90 +43,91 @@ export default class Methods {
               </label>
             </div>
             <p class="card-body__likes like-text"> <span id="span-${i}">${liked}</span> likes</p>`;
-      movieList.appendChild(li);
-      const item = {
-        id: i,
-        title: anime.title,
-        synopsis: anime.synopsis,
-        image: anime.image,
-        likes: liked,
-        comments: 0,
-      };
-      this.animes.push(item);
-      this.itemsCounter();
-    });
-  }
+			movieList.appendChild(li);
+			const item = {
+				id: i,
+				title: anime.title,
+				synopsis: anime.synopsis,
+				image: anime.image,
+				likes: liked,
+				comments: 0,
+			};
+			this.animes.push(item);
+			this.itemsCounter();
+		});
+	}
 
-  increaseLikes(index) {
-    const likeSpan = document.getElementById(`span-${index}`);
-    this.animes[index].likes += 1;
-    likeSpan.innerText = this.animes[index].likes;
-  }
+	increaseLikes(index) {
+		const likeSpan = document.getElementById(`span-${index}`);
+		this.animes[index].likes += 1;
+		likeSpan.innerText = this.animes[index].likes;
+	}
 
-  itemsCounter() {
-    const animesNum = document.getElementById('animesNum');
-    animesNum.innerText = this.animes.length;
-  }
+	itemsCounter() {
+		const animesNum = document.getElementById("animesNum");
+		animesNum.innerText = this.animes.length;
+	}
 
-  async loadModalInfo(index, commentsAPI) {
-    const anime = this.animes[index];
-    this.animes[index].comments = 0;
-    document.getElementById('commentsNumber').innerText = this.animes[index].comments;
-    document.getElementById('animeDescription').innerText = anime.synopsis;
-    document.getElementById('modal-title').innerText = anime.title;
-    document.getElementById('modal-image').setAttribute('src', anime.image);
-    document.getElementById('exampleModalLabel').innerText = anime.title;
-    const comments = document.querySelector('.comments');
-    comments.id = `anime-${index}`;
-    comments.innerHTML = '';
-    const commentsURL = `${commentsAPI}?item_id=${index}`;
-    const commentsArray = await getComments(commentsURL);
-    if (Array.isArray(commentsArray)) {
-      commentsArray.forEach((comment) => {
-        this.addComment(
-          index,
-          comment.username,
-          comment.creation_date,
-          comment.comment,
-        );
-      });
-    }
-  }
+	async loadModalInfo(index, commentsAPI) {
+		const anime = this.animes[index];
+		this.animes[index].comments = 0;
+		document.getElementById("commentsNumber").innerText =
+			this.animes[index].comments;
+		document.getElementById("animeDescription").innerText = anime.synopsis;
+		document.getElementById("modal-title").innerText = anime.title;
+		document.getElementById("modal-image").setAttribute("src", anime.image);
+		document.getElementById("exampleModalLabel").innerText = anime.title;
+		const comments = document.querySelector(".comments");
+		comments.id = `anime-${index}`;
+		comments.innerHTML = "";
+		const commentsURL = `${commentsAPI}?item_id=${index}`;
+		const commentsArray = await getComments(commentsURL);
+		if (Array.isArray(commentsArray)) {
+			commentsArray.forEach((comment) => {
+				this.addComment(
+					index,
+					comment.username,
+					comment.creation_date,
+					comment.comment
+				);
+			});
+		}
+	}
 
-  addComment(index, username, creationDate, comment) {
-    const card = document.createElement('div');
-    const comments = document.querySelector('.comments');
-    const commentsNum = document.getElementById('commentsNumber');
-    card.className = 'card';
-    card.innerHTML = `<div class="card">
+	addComment(index, username, creationDate, comment) {
+		const card = document.createElement("div");
+		const comments = document.querySelector(".comments");
+		const commentsNum = document.getElementById("commentsNumber");
+		card.className = "card";
+		card.innerHTML = `<div class="card">
                   <div class="comment-head">
                   <h5 class="username">${username}</h5>
                   <span class="date">${creationDate}</span>
                   </div>
                   <p class="comment-block">${comment}</p>
                 </div>`;
-    this.animes[index].comments += 1;
-    commentsNum.innerText = this.animes[index].comments;
-    comments.appendChild(card);
-  }
+		this.animes[index].comments += 1;
+		commentsNum.innerText = this.animes[index].comments;
+		comments.appendChild(card);
+	}
 
-  async addNewComment(commentsAPI) {
-    const username = document.getElementById('InputName');
-    const comment = document.getElementById('commentToPost');
-    const container = document.querySelector('.comments');
-    const index = container.id.substring(6, container.id.length);
-    const currentDate = new Date();
-    const month = String(currentDate.getMonth() + 1).padStart(2, '0');
-    const day = String(currentDate.getDate() + 1).padStart(2, '0');
-    const url = `${commentsAPI}?item_id=${index}`;
-    await postComments(index, username.value, comment.value, url);
-    this.addComment(
-      index,
-      username.value,
-      `${currentDate.getFullYear()}-${month}-${day}`,
-      comment.value,
-    );
-    username.value = '';
-    comment.value = '';
-  }
+	async addNewComment(commentsAPI) {
+		const username = document.getElementById("InputName");
+		const comment = document.getElementById("commentToPost");
+		const container = document.querySelector(".comments");
+		const index = container.id.substring(6, container.id.length);
+		const currentDate = new Date();
+		const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+		const day = String(currentDate.getDate() + 1).padStart(2, "0");
+		const url = `${commentsAPI}?item_id=${index}`;
+		await postComments(index, username.value, comment.value, url);
+		this.addComment(
+			index,
+			username.value,
+			`${currentDate.getFullYear()}-${month}-${day}`,
+			comment.value
+		);
+		username.value = "";
+		comment.value = "";
+	}
 }
